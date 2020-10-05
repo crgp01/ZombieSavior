080ba1c44b51e4a47369bd6577accad4bea95529
Cristina Rivera <crivera@thoughtworks.com>
2020-10-05 20:43:08 -0300

    Fix terrain size and add houses

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 100), ""+1.0f / Time.smoothDeltaTime);
+    }
 
     private Rigidbody rb;
     // Start is called before the first frame update
