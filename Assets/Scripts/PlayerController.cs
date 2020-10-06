752759bbcd52c7fc22909ff2e2f358020c6a2a4d
Cristina Rivera <crivera@thoughtworks.com>
2020-10-05 21:17:33 -0300

    Add graveyard

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void OnGUI()
-    {
-        GUI.Label(new Rect(0, 0, 100, 100), ""+1.0f / Time.smoothDeltaTime);
-    }
-
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
