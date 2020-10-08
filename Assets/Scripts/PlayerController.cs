3d9aba88121bf9fad28577d3713bd5d0e2ae1083
Cristina Rivera <crivera@thoughtworks.com>
2020-10-07 23:09:07 -0300

    Archie is picking objects and saying hello

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private ScoreManager scoreManager;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,6 +20,18 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             scoreManager.recolectedCoins++;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            animator.Play("Pickup");
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            animator.Play("Wave");
         }
     }
 
