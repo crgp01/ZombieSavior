9aaed9442ac29f9e16ce328702bd45ac9551c9c0
Cristina Rivera <crivera@thoughtworks.com>
2020-11-04 10:27:36 -0300

    Add yellow potion logic

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -18,6 +18,8 @@ public class InventoryController : MonoBehaviour
     public ScoreManager scoreManager;
     private string COMPLETE_LIFE_MESSAGE = "Ya tienes la vida completa";
     private string NO_POTION_MESSAGE = "No tienes mas pociones";
+    private string NO_ZOMBIE_MODE_MESSAGE = "El contador no está corriendo";
+    private string ZERO_COUNTER_MESSAGE = "El contador ya está en 0";
     // Start is called before the first frame update
     void Start()
     {
@@ -54,5 +56,34 @@ public class InventoryController : MonoBehaviour
             errorPanel.text = COMPLETE_LIFE_MESSAGE;
         }
         
+    }
+    public void ShowYellowPotionPanel()
+    {
+        greenPotionPanel.SetActive(false);
+        yellowPotionPanel.SetActive(true);
+        shotgunPanel.SetActive(false);
+        pistolPanel.SetActive(false);
+        diaryPanel.SetActive(false);
+        coinsPanel.SetActive(false);
+    }
+    public void UseYellowPotionPanel()
+    {
+        if (scoreManager.zombieMode)
+        {
+            if (scoreManager.timeRemaining > 1)
+            {
+                storeController.greenPotionCounter--;
+                scoreManager.lifeBarSlider.value = 3;
+            }
+            else
+            {
+                errorPanel.text = ZERO_COUNTER_MESSAGE;
+            }
+        }
+        else
+        {
+            errorPanel.text = NO_ZOMBIE_MODE_MESSAGE;
+        }
+
     }
 }
