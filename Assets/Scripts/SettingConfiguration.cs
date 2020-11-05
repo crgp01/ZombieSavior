b7407a095d73ae33800daa223c5f078045b7c6b2
Cristina Rivera <crivera@thoughtworks.com>
2020-11-05 13:50:44 -0300

    Add audiomixer for general music

---
+++ Assets/Scripts/SettingConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SettingConfiguration : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    // Start is called before the first frame update
+    public void SetMusicVolume(float volume) {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+}
